7fad02da9af3df9772a0a3622d173f76eda9699f
Chloe Krainbucher <chloekrainbucher@pOiuytr-easd-bh78oohb8ubh8.local>
2017-03-21 19:27:49 -0400

    added wont let you input invalid word

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require_relative 'hangman'
 
+enable :sessions
+
 get '/' do
 	erb :names
 end
@@ -11,14 +13,36 @@ post '/names' do
 	erb :secret_word, :locals => {:p1 => backend_p1_name, :p2 => backend_p2_name}
 end
 
+get '/secret_word' do
+	backend_p1_name = params[:backend_p1_name]
+	backend_p2_name = params[:backend_p2_name]
+	erb :secret_word, :locals => {:p1 => backend_p1_name, :p2 => backend_p2_name}
+end
+
+
 post '/secret_word' do
-	backend_secret_word = params[:secret_word_input]
+	secret_word = params[:secret_word_input]
+	backend_secret_word = Word.new(params[:secret_word_input])
 	backend_p1_name = params[:p1_name_input]
 	backend_p2_name = params[:p2_name_input]
-	underscores = backend_secret_word.gsub(/[abcdefghijklmnopqrstuvwxyz]/, '_ ')
+	underscores = secret_word.gsub(/[abcdefghijklmnopqrstuvwxyz]/, '_ ')
 	underscores.chars
-	erb :guess, :locals => {:underscores => underscores, :secret => backend_secret_word, :p1 => backend_p1_name, :p2 => backend_p2_name}
+	countdown = 6
+
+		if backend_secret_word.valid_input? == false
+			redirect '/secret_word?backend_p1_name=' + backend_p1_name + '&backend_p2_name=' + backend_p2_name
+		end
+
+	erb :guess, :locals => {:countdown => countdown, :underscores => underscores, :secret => backend_secret_word, :p1 => backend_p1_name, :p2 => backend_p2_name}
 end
+
+
+# post '/guess' do
+
+# 		if letter_guess?()
+
+
+
 	
 
 
