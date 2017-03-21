213bcc708979581321988977809815d13db89ab0
Chloe Krainbucher <chloekrainbucher@pOiuytr-easd-bh78oohb8ubh8.local>
2017-03-21 12:58:46 -0400

    app show underscores and input guess

--- app.rb
+++ app.rb
@@ -13,6 +13,12 @@ end
 
 post '/secret_word' do
 	backend_secret_word = params[:secret_word_input]
+	backend_p1_name = params[:p1_name_input]
+	backend_p2_name = params[:p2_name_input]
+	underscores = backend_secret_word.gsub(/[abcdefghijklmnopqrstuvwxyz]/, '_ ')
+	underscores.chars
+	erb :guess, :locals => {:underscores => underscores, :secret => backend_secret_word, :p1 => backend_p1_name, :p2 => backend_p2_name}
+end
 	
 
 
